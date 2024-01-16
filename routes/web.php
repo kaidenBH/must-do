03cb42009080ba62253bdaa87c521edd04595f27
yasuoPi <haitem.bourahla7@gmail.com>
2024-01-16 13:41:09 +0100

    added tasks models, migration and routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Return_;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => \App\Models\Task::latest()->get()
+    ]);
+})->name('tasks.index');
+
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', [
+        'task' => \App\Models\Task::findOrFail($id)
+    ]);
+})->name('tasks.show');
+
+Route::fallback(function () {
+    return abort(Response::HTTP_NOT_FOUND);;
 });
