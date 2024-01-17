1942a01c8d5738bb3f14ab9b94c9f96337597c1d
yasuoPi <haitem.bourahla7@gmail.com>
2024-01-17 18:41:31 +0100

    modified the routes a bit and added form to create tasks and save them to the backend

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use \App\Models\Task;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Stmt\Return_;
 
@@ -21,16 +24,38 @@ Route::get('/', function() {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->get()
+        'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show', [
-        'task' => \App\Models\Task::findOrFail($id)
+        'task' => Task::findOrFail($id)
     ]);
 })->name('tasks.show');
+
+Route::post('/tasks', function(Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task created successfully!');
+})->name('tasks.store');
+
+
 
 Route::fallback(function () {
     return abort(Response::HTTP_NOT_FOUND);;
 });
+
