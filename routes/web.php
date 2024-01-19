87c6f0a1a91f8dd4f527a7837b9b0d639fe559f1
yasuoPi <haitem.bourahla7@gmail.com>
2024-01-19 12:28:17 +0100

    added the edit functionality to tasks and refractored the routes a bit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use \App\Models\Task;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
@@ -30,30 +31,37 @@ Route::get('/tasks', function () {
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id) {
+Route::get('/tasks/{task}', function (Task $task) {
     return view('show', [
-        'task' => Task::findOrFail($id)
+        'task' => $task
     ]);
 })->name('tasks.show');
 
-Route::post('/tasks', function(Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task
     ]);
+})->name('tasks.edit');
 
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+Route::post('/tasks', function(TaskRequest $request) {
+    $task = Task::create($request->validated());
 
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
+Route::put('/tasks/{task}', function(Task $task, TaskRequest $request) {
+    $task->update($request->validated());
 
+    return redirect()->route('tasks.show', ['task' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
+
+Route::delete('/tasks/{task}', function(Task $task) {
+    $task->delete();
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
 
 Route::fallback(function () {
     return abort(Response::HTTP_NOT_FOUND);;
